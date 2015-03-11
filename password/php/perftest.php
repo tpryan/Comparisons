926a4f32841119b021d1500599d25727b7a892d9
Terrence Ryan <terry@terrenceryan.com>
2015-03-11 16:42:43 -0700

    Writing various tester rigs for pho version.

---
+++ password/php/perftest.php
@@ -0,0 +1,34 @@
+<?php
+	
+	include_once("password/php/rules.php");
+	$rules = new Rules();
+
+	if (defined('STDIN') && array_key_exists(1, $argv)) {
+	  $loopcount = $argv[1];
+	} else { 
+	  $loopcount = 1;
+	}
+
+	$i=1;
+
+	$handle = fopen("password/data/test_passwords.txt", "r");
+	if ($handle) {
+    	while (($line = fgets($handle)) !== false) {
+    		if ($i>$loopcount){
+    			break;
+    		}
+    		$result = $rules->validate($line);
+    		//echo $line . " " . implode(" ",$result) . "\n";
+
+    		$i++;
+	    }
+
+	    fclose($handle);
+	} else {
+	    echo "Error opening test file";
+	} 
+
+
+
+
+?>
